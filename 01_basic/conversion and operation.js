5cd79927b1e33d77f965781461bac39c137dbdb1
dev-avaneesh <avaneesh9500@gmail.com>
2024-12-25 13:26:37 +0530

    done

---
+++ 01_basic/conversion and operation.js
@@ -0,0 +1,29 @@
+let score="56436acdvh"
+console.log(typeof score)
+console.log(typeof (score))
+
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber)
+
+// let valueInString=String(score)
+// console.log(typeof valueInString)
+
+console.log(valueInNumber)
+
+//"33"=>33
+//33=>NaN
+//true=>1; false=>0
+
+let isLoggedIn=1
+let booleanIsLoggedIn=Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn)
+
+
+//**********Operations************/
+
+//console.log(2+2)
+//console.log(2-2)
+//console.log(2*2)
+//console.log(2/2)
+//console.log(2%2)
+//console.log(2**2)
