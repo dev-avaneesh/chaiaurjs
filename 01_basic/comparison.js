cb0a525680c1dd20789aeb8bfa123d03b8d3d90b
dev-avaneesh <avaneesh9500@gmail.com>
2024-12-25 13:46:29 +0530

    done

---
+++ 01_basic/comparison.js
@@ -0,0 +1,22 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+//console.log(null > 0);
+//console.log(null == 0);
+//console.log(null >= 0);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+// console.log("2" > 1);
+
+console.log(undefined > 0);
+console.log(undefined == 0);
+console.log(undefined < 0);
+
+
+// === =>value and type dono check krta hai but dono value same honi chahiye
