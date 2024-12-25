0607c3d68eec314d825fabaa8fbd3854d4ed738a
dev-avaneesh <avaneesh9500@gmail.com>
2024-12-25 12:52:52 +0530

    done

---
+++ 01_basic/data.js
@@ -0,0 +1,18 @@
+let number = 10
+let string = "Avaneesh"
+let boolean = true
+let state;
+
+//data types in javascript
+//1. Number
+//2. String
+//3. Boolean
+//4. Undefined
+//5. Null
+//6. Symbol
+//7. BigInt
+
+//object
+console.log(typeof "Avaneesh");
+console.log(typeof 10);
+console.log(typeof null);
