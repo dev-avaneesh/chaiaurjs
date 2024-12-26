707992b99745095be29dd0b53997d86f042cab69
dev-avaneesh <avaneesh9500@gmail.com>
2024-12-26 12:55:30 +0530

    done

---
+++ 01_basic/data types summary.js
@@ -0,0 +1,25 @@
+// there are two types of data types in java script
+
+//1.primitive data types 
+//7 types: String,Number,Boolean,Undefined,Null,BigInt,Symbol
+
+const BigNumber=7312496463897123789379n
+
+//2. non -primitive data types
+//object,Array,Function 
+
+const heros = ["superman, batman, spiderman"];
+
+let myObj={
+    name:"Avaneesh",
+    age:25,
+    }
+
+    const myFunction=function(){
+        console.log("Avaneesh kumar Singh");
+    }
+
+    console.log(typeof myFunction);
+    console.log(typeof myObj);
+    console.log(typeof heros);
+    console.log(typeof BigNumber);
