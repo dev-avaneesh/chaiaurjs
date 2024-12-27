88e2432bd7df3264baecf6e37c0c7888a684fe07
dev-avaneesh <avaneesh9500@gmail.com>
2024-12-27 16:15:54 +0530

    done

---
+++ 01_basic/Array.js
@@ -0,0 +1,48 @@
+// array
+
+const myArr= [1,2,3,4,5,6,7,8];
+const myHeros=['ironman','caption america']
+// console.log(myArr);
+
+const myArr2 = new Array(1,2,3,4,5,6,7,8);
+// console.log(myArr2[1]);
+
+//array methods
+
+//push method (iska use karke hum kisi bhi array me koi bhi value add ker sakte hai)
+myArr.push(9);
+// console.log(myArr);
+
+//Pop method (iska use karke hum kisi bhi array me last value ko remove kar dete hai)
+myArr.pop(9);
+// console.log(myArr);
+
+// unshift method (iska use karke hum kisi bhi array me kisi bhi value ko startme add kar sakte hai)
+myArr.unshift(11);
+// console.log(myArr);
+
+//shift method (iska use karke hum kisi bhi array me kisi bhi value ko start me remove kar sakte hai)
+myArr.shift(11);
+// console.log(myArr);
+
+// console.log(myArr.indexOf(5));
+
+// join method ( is method ka use hum kisi bhi array ko join kar sakte hai)
+
+// console.log(myArr);
+// console.log(myArr2);
+
+
+//slice method(iska use karke hum kisi bhi array ko slice kar sakte hai
+
+const myArray=[1,2,3,4,5,5,6,7,8]
+console.log(myArray.slice(1,4));
+
+//splice method (splice method ka use karke hum kisi bhi array me kisi bhi value ko add kar sakte hai)
+
+const myArray2=[1,2,3,4,5,6,7,8]
+console.log(myArray2.splice(1,4))
+
+
+
+
