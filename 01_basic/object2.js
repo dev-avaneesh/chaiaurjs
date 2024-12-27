6173033465722ab95dcb741ee5f592875a9fad74
dev-avaneesh <avaneesh9500@gmail.com>
2024-12-27 22:07:00 +0530

    done

---
+++ 01_basic/object2.js
@@ -0,0 +1,12 @@
+const course={
+    coursename:"js in hindi",
+    price:"999",
+    courseInstructor:"Avaneesh"
+}
+
+// course.courseInstructor
+
+const {courseInstructor: instructor}= course //{}iska matlab distructuring hota hai
+// console.log(courseInstructor);
+
+console.log(instructor);
